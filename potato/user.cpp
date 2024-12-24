dda9543614256ed616c1a0c334c72b8c51ff51a2
mincoding <74457149+mincoding1@users.noreply.github.com>
2024-12-24 12:52:05 +0900

    Create user.cpp

---
+++ potato/user.cpp
@@ -0,0 +1,14 @@
+/*** user.cpp ***/
+#define MAX_N 100
+
+void init(int N, int mField[MAX_N][MAX_N]) {
+	return;
+}
+ 
+int dropSeed(int mType, int mRow, int mCol, int mEnergy){
+	return 0;
+}
+ 
+int dropPotion(int mRow, int mCol){
+	return 0;
+}
