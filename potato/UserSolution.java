86cc1644d000c0cc91137490f2399705d9d2891e
mincoding <74457149+mincoding1@users.noreply.github.com>
2024-12-24 12:52:44 +0900

    Create UserSolution.java

---
+++ potato/UserSolution.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class UserSolution {
+	
+	public void init(int N, int mField[][]) {
+		return; 
+	}
+	
+	public int dropSeed(int mType, int mRow, int mCol, int mEnergy) {
+		return 0; 
+	}
+	
+	public int dropPotion(int mRow, int mCol) {
+		return 0; 
+	}
+}
