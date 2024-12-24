10730f4c93e4f976fd1ad6edff47e386afdaddae
mincoding <74457149+mincoding1@users.noreply.github.com>
2024-12-24 14:34:39 +0900

    Update Main.java

--- find/Main.java
+++ find/Main.java
@@ -2,70 +2,85 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    private final static int CMD_INIT 			= 100;
+    private final static int CMD_DROPSEED 		= 200;
+    private final static int CMD_DROPPOTION		= 300;
+    private final static int MAX_N 				= 100;
 
-	private final static int CMD_ADD = 1;
-	private final static int CMD_FIND = 2;
+    private final static UserSolution usersolution = new UserSolution();
 
-	private static boolean run(BufferedReader br) throws IOException {
+    private static boolean run(BufferedReader br) throws Exception {
 
-		StringTokenizer st;
+        StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
+        int Q;
+        st = new StringTokenizer(br.readLine());
+        Q = Integer.parseInt(st.nextToken());
 
-		usersolution.init(); 
+        int userAns, ans;
+        int N, row, col;
+        int type, energy;
+        boolean isCorrect = false;
+        int[][] field = new int[MAX_N][MAX_N];
+        int cmd;
 
-		int id;
-		String name;
-		String userAns;
-		String ans;
-		boolean isCorrect = true;
+        for (int q = 0; q < Q; ++q) {
+            st = new StringTokenizer(br.readLine());
+            cmd = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int cmd = Integer.parseInt(st.nextToken());
-			switch (cmd) 
-			{
-				case CMD_ADD:
-					id = Integer.parseInt(st.nextToken());
-					name = st.nextToken();
-					usersolution.add(id, name);
-					break; 
-	
-				case CMD_FIND:
-					ans = st.nextToken();
-					id = Integer.parseInt(st.nextToken());
-					userAns = usersolution.find(id);
-					if (!userAns.equals(ans)) {
-						isCorrect = false;
-					}
-					break;
+            switch (cmd) {
+                case CMD_INIT:
+                    N = Integer.parseInt(st.nextToken());
+                    for(int i = 0; i < N; i++) {
+                        st = new StringTokenizer(br.readLine());
+                        for(int j = 0; j < N; j++)
+                            field[i][j] = Integer.parseInt(st.nextToken());
+                    }
+                    usersolution.init(N, field);
+                    isCorrect = true;
+                    break;
+                case CMD_DROPSEED:
+                    type = Integer.parseInt(st.nextToken());
+                    row = Integer.parseInt(st.nextToken());
+                    col = Integer.parseInt(st.nextToken());
+                    energy = Integer.parseInt(st.nextToken());
+                    userAns = usersolution.dropSeed(type, row, col, energy);
+                    ans = Integer.parseInt(st.nextToken());
+                    if(userAns != ans)
+                        isCorrect = false;
+                    break;
+                case CMD_DROPPOTION:
+                    row = Integer.parseInt(st.nextToken());
+                    col = Integer.parseInt(st.nextToken());
+                    userAns = usersolution.dropPotion(row, col);
+                    ans = Integer.parseInt(st.nextToken());
+                    if (userAns != ans)
+                        isCorrect = false;
+                    break;
+                default:
+                    isCorrect = false;
+                    break;
+            }
+        }
+        return isCorrect;
+    }
 
-				default:
-					isCorrect = false;
-					break;
-			}
-		}
-		return isCorrect;
-	}
+    public static void main(String[] args) throws Exception {
+        int TC, MARK;
 
-	private final static UserSolution usersolution = new UserSolution();
+        System.setIn(new java.io.FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		// System.setIn(new java.io.FileInputStream("res/sample_input.txt"));
+        TC = Integer.parseInt(st.nextToken());
+        MARK = Integer.parseInt(st.nextToken());
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int TC = Integer.parseInt(st.nextToken());
-		int MARK = Integer.parseInt(st.nextToken());
+        for (int testcase = 1; testcase <= TC; ++testcase) {
+            int score = run(br) ? MARK : 0;
+            System.out.println("#" + testcase + " " + score);
+        }
 
-		for (int testcase = 1; testcase <= TC; testcase++) {
-			int score = run(br) ? MARK : 0;
-			System.out.println("#" + testcase + " " + score);
-		}
-
-		br.close();
-	}
+        br.close();
+    }
 }
