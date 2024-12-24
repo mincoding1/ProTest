8950b04559ef44304dc21e5274ce7997f62cd241
mincoding <74457149+mincoding1@users.noreply.github.com>
2024-12-24 10:24:19 +0900

    Update and rename Main.java to UserSolution.java

---
+++ find/UserSolution.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class UserSolution
+{
+	public void init()
+	{
+	}
+
+	public void add(int id, String name)
+	{
+	}
+
+	String find(int id) 
+    {
+		return ""; 
+	}
+}
