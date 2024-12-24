b3006d80df8480453240edbe2599fa084580769c
mincoding <74457149+mincoding1@users.noreply.github.com>
2024-12-24 09:34:56 +0900

    Create user.java

---
+++ user.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+class UserSolution {
+	
+	public int sum(int A, int B) {
+		return 0;
+	} 
+	
+
+	public int sub(int A, int B) {
+		return 0;
+	} 
+
+	public int mul(int A, int B) {
+		return 0;
+	} 
+
+	public int div(int A, int B) {
+		return 0;
+	} 
+	
+}
